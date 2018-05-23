9807295d9f6f7ff9c34b3f9742c5f44b71d77c2c
Endrick <Endrick@DESKTOP-CQTE0GH>
2018-05-23 16:10:22 -0300

    Cor do texto

--- js/app.js
+++ js/app.js
@@ -19,17 +19,14 @@ $('nav a').click(function(e){
 		//RESPONSIVIDADE -- CORES E TAMANHOS
     	$(window).on('resize', function() {
 		  var win = $(this);
-		  if (win.width() <= 300) {
+		  if (win.width() <= 400) {
 
 		    $('#123').addClass('black-text');
 		    $('#123').removeClass('white-text');
-		    $('#bg1').addClass('bg-small');
-		    $('#bg1').removeClass('bg-large');
-
 		  } else {
+
 		    $('#123').addClass('white-text');
 		    $('#123').removeClass('black-text');
-		    
 		  }
 
 		  if (win.width() <= 430) {
